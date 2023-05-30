a735306181046a63414d1e82ecaa781742d374a6
domesticwarlord86 <domesticwarlord86@gmail.com>
2023-05-30 15:46:37 -0500

    Update Extractor.cs

--- Extractor/Extractor.cs
+++ Extractor/Extractor.cs
@@ -87,12 +87,8 @@ namespace Extractor
 
         internal static async Task ExtractMateria()
         {
-            if (InventoryManager.FilledInventoryAndArmory.Any(x => x.SpiritBond == 100f && x.RawItemId != 7873 || x.RawItemId != 7874 || x.RawItemId != 7875 || x.RawItemId != 7876 || x.RawItemId != 7877 || x.RawItemId != 7878 || x.RawItemId != 7879 || x.RawItemId != 7880 || x.RawItemId != 7881 || x.RawItemId != 7882 || x.RawItemId != 9255 ) && InventoryManager.FreeSlots > 0)
-            {
-                await LlamaLibrary.Helpers.GeneralFunctions.StopBusy(leaveDuty: false);
-                Log.Information($"Extracting Materia from gear");
-                await LlamaLibrary.Utilities.Inventory.ExtractFromAllGear();
-            }
+            List<uint> RelicSphereScrolls = new() { 7873, 7874, 7875, 7876, 7877, 7878, 7879, 7880, 7881, 7882, 9255 };
+            if (InventoryManager.FilledInventoryAndArmory.Any(x => x.SpiritBond == 100f && RelicSphereScrolls.Contains(x.RawItemId) == false) && InventoryManager.FreeSlots > 0)
         }        
 
     }
